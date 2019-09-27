ce49f92a02521c2d39e35133cd44711a7e6b6d91
janadari <janadari1995@gmail.com>
2019-09-27 17:36:52 +0530

    Add existing file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularForm';
+
+
+  topics= ['Angular', 'React',' Vue'];
+
+  userModel  = new User('janadari','janadari1995@gmail.com',41417,'','morning',true);
 }
